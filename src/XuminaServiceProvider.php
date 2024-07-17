b761ac3b9455877c1ad05b9ee67fb2cc2c867894
Hasnayeen <searching.nehal@gmail.com>
2024-07-17 19:00:38 +0600

    Add package version in about command

--- src/XuminaServiceProvider.php
+++ src/XuminaServiceProvider.php
@@ -3,12 +3,14 @@
 namespace Hasnayeen\Xumina;
 
 use App\Http\Middleware\HandleInertiaRequests;
+use Composer\InstalledVersions;
 use Hasnayeen\Xumina\Commands\InstallCommand;
 use Hasnayeen\Xumina\Commands\PanelCommand;
 use Hasnayeen\Xumina\Commands\ResourceCommand;
 use Hasnayeen\Xumina\Commands\SyncCommand;
 use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Console\AboutCommand;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 use Spatie\LaravelPackageTools\Package;
@@ -39,7 +41,7 @@ class XuminaServiceProvider extends PackageServiceProvider
     public function register(): void
     {
         parent::register();
-        $this->app->singleton(Xumina::class, fn (Application $app) => new Xumina);
+        $this->app->singleton(Xumina::class, fn(Application $app) => new Xumina);
         $this->app->alias(Xumina::class, 'xumina');
     }
 
@@ -51,23 +53,32 @@ class XuminaServiceProvider extends PackageServiceProvider
         parent::boot();
         $xumina = app('xumina');
         Route::middleware(['web', HandleInertiaRequests::class])
-            ->group(__DIR__.'/../routes/web.php');
+            ->group(__DIR__ . '/../routes/web.php');
         if (request()->segments()) {
             $xumina->getPanels()
                 ->each(function (Panel $panel) use ($xumina) {
                     if (request()->segments()[0] === $panel->getPrefix()) {
                         $xumina->currentPanel($panel->getName());
-                        Authenticate::redirectUsing(fn () => route('xumina.'.Str::kebab($panel->getName()).'.auth.login'));
+                        Authenticate::redirectUsing(fn() => route('xumina.' . Str::kebab($panel->getName()) . '.auth.login'));
 
                         return false;
                     }
                     if (in_array($panel->getPrefix(), ['', '/'])) {
                         $xumina->currentPanel($panel->getName());
-                        Authenticate::redirectUsing(fn () => route('xumina.'.Str::kebab($panel->getName()).'.auth.login'));
+                        Authenticate::redirectUsing(fn() => route('xumina.' . Str::kebab($panel->getName()) . '.auth.login'));
 
                         return false;
                     }
                 });
+        }
+    }
+
+    public function packageBooted(): void
+    {
+        if (class_exists(AboutCommand::class) && class_exists(InstalledVersions::class)) {
+            AboutCommand::add('Xumina', [
+                'Version' => InstalledVersions::getPrettyVersion('hasnayeen/themes'),
+            ]);
         }
     }
 }
